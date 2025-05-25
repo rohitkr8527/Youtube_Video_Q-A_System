9370d0bbfef80ae19bd2b307d4b2462481f2843f
rohitkr8527 <rohitkr7518@gamil.com>
2025-05-26 00:28:27 +0530

    changed vector db from chromadb to faiss

--- vector_store_utils.py
+++ vector_store_utils.py
@@ -1,9 +1,9 @@
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 
 def create_vector_store(chunks):
     embedding_model = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 
-    # Create Chroma vector store in-memory (no persist_directory)
-    vectordb = Chroma.from_texts(texts=chunks, embedding=embedding_model)
+    # Create FAISS vector store in-memory (FAISS doesn't require SQLite)
+    vectordb = FAISS.from_texts(texts=chunks, embedding=embedding_model)
     return vectordb
